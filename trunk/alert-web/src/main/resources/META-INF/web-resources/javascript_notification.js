e88f99433ea04ae4dd7e5633143982061e2813bc
tranhuynghi.it@gmail.com <tranhuynghi.it@gmail.com@64858aac-1bc0-a650-e873-744462af0a14>
2013-11-08 09:43:46 +0000

    notification 8/11/2013

--- trunk/alert-web/src/main/resources/META-INF/web-resources/javascript_notification.js
+++ trunk/alert-web/src/main/resources/META-INF/web-resources/javascript_notification.js
@@ -20,56 +20,77 @@
 //			</ul>
 //		{{/header_notification_bar}}
 
-//var Notification = can.Model.extend({
-//    findAll: 'GET /notification',
-//    findOne: 'GET /notification/{id}',
-//    create:  function( attrs ){
-//        return $.ajax("/notification/",{
-//        	type: "POST",
-//        	processData: false,
-//        	data: JSON.stringify(attrs),
-//        	dataType: 'json',
-//        	contentType: "application/json; charset=utf-8"
-//        });
-//    },
-////    update: "/notification/{id}",
-//    update:  function(id, attrs ){
-//        return $.ajax("/notification/" + id,{
-//        	type: "PUT",
-//        	processData: false,
-//        	data: JSON.stringify(attrs),
-//        	dataType: 'json',
-//        	contentType: "application/json; charset=utf-8"
-//        });
-//    },
-//    destroy:"/notification/{id}" 
-//    
-//}, {});
-//
-//var Badge = can.Control.extend({
-//	"init" : function(element, options) {
-//		var el = $("id_badge");
-//		Notification.findAll({}, function(notification){
-//			el.
-//		});
-//		
-//	}
-//});
-//
-//
-//var Routing = can.Control.extend({
-//	"init" : function() {
-//		this.id_badge = new Badge("#id_badge");
-//	}
-//});
-//
-//new Routing(document.body);
-//can.route.ready()
+var Notification = can.Model.extend({
+    findAll: 'GET /notification',
+    findOne: 'GET /notification/{id}',
+    create:  function( attrs ){
+        return $.ajax("/notification/",{
+        	type: "POST",
+        	processData: false,
+        	data: JSON.stringify(attrs),
+        	dataType: 'json',
+        	contentType: "application/json; charset=utf-8"
+        });
+    },
+//    update: "/notification/{id}",
+    update:  function(id, attrs ){
+        return $.ajax("/notification/" + id,{
+        	type: "PUT",
+        	processData: false,
+        	data: JSON.stringify(attrs),
+        	dataType: 'json',
+        	contentType: "application/json; charset=utf-8"
+        });
+    },
+    destroy:"/notification/{id}" 
+    
+}, {});
 
-var data = new can.Map({
-    user: 'Tina Fey',
-    messages: 0
+var Notification_bar = can.Control.extend({
+	"init" : function(element, options) {
+		var el = $("#header_notification_bar");
+		var el2 = $("#id_badge");
+		Notification.findAll({}, function(notifications){
+			console.log(notifications.length),
+			console.log(notifications)
+			
+			var data = new can.Map({
+			    number_notification: notifications.length,
+			});	
+			el2.html(can.view("template", {id_badge : data}));
+			el.html(can.view("template", {header_notification_bar : notifications}));
+		})
+	}
 });
 
-var template = can.view("#template", data)
-document.body.appendChild(template);
+var Badge = can.Control.extend({
+
+});
+
+var Routing = can.Control.extend({
+	"init" : function() {
+		//new Badge("#id_badge");
+		new Notification_bar("#header_notification_bar");
+	}
+});
+
+function toggle_visibility(id) {
+    var e = document.getElementById(id);
+    if(e.style.display == 'block')
+       e.style.display = 'none';
+    else
+       e.style.display = 'block';
+ }
+
+function hide_visibility(id) {
+    var e = document.getElementById(id);
+    e.style.display = 'none';
+ }
+
+function show_visibility(id) {
+    var e = document.getElementById(id);
+    e.style.display = 'block';
+ }
+
+new Routing(document.body);
+can.route.ready()
